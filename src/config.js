f1a971ad093131112935bd7520707828529bb3b2
Cham <ychamam7@gmail.com>
2021-12-01 01:18:05 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const layersOrder = [
+    { name: 'background', number: 5 },
+    { name: 'body', number: 5 },
+    { name: 'eyes', number: 5 },
+    { name: 'hat', number: 5 },
+    { name: 'mouth', number: 5 },
+    { name: 'shirt', number: 5 }
+];
+  
+const format = {
+    width: 4500,
+    height: 4500
+};
+
+
+const defaultEdition = 10;
+
+module.exports = { layersOrder, format, defaultEdition };
